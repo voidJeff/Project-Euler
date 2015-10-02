6e9691e689a8ae7b3405e38ff5ab3a6c6f3bef2a
voidJeff <qy120838@126.com>
2015-10-02 14:59:10 -0400

    Updated from Brython Server: October 2, 2015 2:59:10 PM EDT

--- Sum of Fibonacci.py
+++ Sum of Fibonacci.py
@@ -1,4 +1,8 @@
+i = 0
 n_l = [1, 2]
 while n_l[len(n_l)-1] + n_l[len(n_l)-2] <= 4000000:
     n_l.append(n_l[len(n_l)-1] + n_l[len(n_l)-2])
-print(n_l)
+while n_l[i]%2 != 0 and i < 4000000:
+    n_l.remove(n_l[i])
+    i += 1
+print(sum(n_l))
