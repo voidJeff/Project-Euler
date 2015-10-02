24f3271300a467ac3d83d0e0ece271b5e5fc4238
voidJeff <qy120838@126.com>
2015-10-02 14:26:35 -0400

    Updated from Brython Server: October 2, 2015 2:26:34 PM EDT

--- SumO'Multiple.py
+++ SumO'Multiple.py
@@ -1,1 +1,9 @@
-
+num = list(range(1, 1001))
+n = 0
+while n < len(num):
+    if num[n]%3 != 0 or num[n]%5 != 0:
+        if num.count(num[n]) != 0:
+            num.remove(num[n])
+    n += 1
+print(num)
+print(sum(num))
