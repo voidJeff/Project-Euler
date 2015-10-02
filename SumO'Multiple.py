d740c86dd4bd90589a75aa28cf81518148042eb1
voidJeff <qy120838@126.com>
2015-10-02 14:46:29 -0400

    Updated from Brython Server: October 2, 2015 2:46:29 PM EDT

--- SumO'Multiple.py
+++ SumO'Multiple.py
@@ -2,12 +2,12 @@ num = list(range(1, 1001))
 n_num = []
 i = 1
 while i*3 < 1000:
-    if num.count(3*i) == 1:
+    if num.count(3*i) == 1 and n_num.count(3*i) != 1:
         n_num.append(3*i)
     i += 1
 i = 1
 while i*5 < 1000:    
-    if num.count(5*i) == 1:
+    if num.count(5*i) == 1 and n_num.count(5*i) != 1:
         n_num.append(5*i)
     i += 1
 print(n_num)
